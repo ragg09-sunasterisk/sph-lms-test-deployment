36d64c1a7e489ec51e9ac61b8e322633e624644b
ragg09-sunasterisk <115448429+ragg09-sunasterisk@users.noreply.github.com>
2023-06-06 17:22:49 +0800

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,8 +3,10 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './features/**/*.{js,ts,jsx,tsx}',
+    './public/**/*.{js,ts,jsx,tsx}',
+    './services/**/*.{js,ts,jsx,tsx}',
+    './styles/**/*.{js,ts,jsx,tsx}',
 
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx}',
